d8fe91d78ae9b9c151731622be1fc4263f427add
mikstef0 <mikolajstefaniszyn@interia.pl>
2023-07-01 00:27:19 +0200

    Unit test for catching SIGTERM added

--- Makefile
+++ Makefile
@@ -7,7 +7,9 @@ ifeq ($(CC),clang)
 	options = $(cla_opt)
 endif
 
-cputr: main.c cut_threads.c cut_threads.h main.h
+cputr: main.c cut_threads.c cut_threads.h main.h global.h
 	$(CC) main.c cut_threads.c $(options) -std=c99 -o cputr
+test: main_test.c cut_threads.c cut_threads.h main.h global.h
+	$(CC) main_test.c $(options) -std=c99 -o test
 clean:
 	rm cputr
---
+++ main_test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+#include <time.h>
+#include <assert.h>
+#include "main.h"
+#include "global.h"
+//#include "cut_threads.h"
+
+
+
+void test(int i)
+{
+    if(i==1) 
+    {
+        fprintf(stdout, "\n** Test stared. Sigterm is being sent. **\n");
+        raise(SIGTERM);
+    }
+    if(i==2)
+    {   
+        assert(terminate==1);
+        fprintf(stdout, "\n** Test PASSED. Sigterm was caught. **\n");
+    }
+}
+
+void term()
+{
+    terminate=1;
+    fprintf(stdout, "\nSIGTERM/SIGINT encountered. Terminating...\n");
+    pthread_cancel(reader_thr);
+    pthread_cancel(analyzer_thr);
+    pthread_cancel(printer_thr);
+    pthread_cancel(watchdog_thr);
+    pthread_join(reader_thr, NULL);
+    pthread_join(analyzer_thr, NULL);
+    pthread_join(printer_thr, NULL);
+    pthread_join(watchdog_thr, NULL);
+
+    free(rd);
+    free(rd_old);
+    free(CPU_Percentage);
+
+    fprintf(stdout, "Program has terminated.\n");
+    test(2);
+    exit(0);
+}
+
+
+
+int main(int argc, char  *argv[])
+{
+    
+    terminate=0;
+    procno = sysconf(_SC_NPROCESSORS_ONLN);
+
+    rd=(struct procdata_struct*)malloc((1+procno)*sizeof(*rd));
+    rd_old=(struct procdata_struct*)malloc((1+procno)*sizeof(*rd_old));
+    CPU_Percentage=(float*)malloc((1+procno)*sizeof(CPU_Percentage));
+
+
+    sa_term.sa_handler=term;
+    sigaction(SIGTERM, &sa_term, NULL);
+    sa_int.sa_handler=term;
+    sigaction(SIGINT, &sa_int, NULL);
+
+ 
+    while(terminate==0)
+    {
+    sleep(1);
+    test(1);
+    }
+
+    
+    return 0;
+}
+
